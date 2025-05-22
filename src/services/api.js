35138ffd885e2d23f25113d056835b0452f99d84
esratrn <122872908+esratrn@users.noreply.github.com>
2025-05-22 10:12:55 +0300

    ss

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://stockly-epfmbzcmd9a9fsg3.westeurope-01.azurewebsites.net/api' // dikkat! /api eklendi
+  baseURL: 'https://stockly-epfmbzcmd9a9fsg3.westeurope-01.azurewebsites.net' // ❌ /api EKLENMEYECEK!
 });
 
-// JWT Token otomatik ekleniyor
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
